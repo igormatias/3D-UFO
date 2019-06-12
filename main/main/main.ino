ee22e0b47bbf266b3a88c4e852d08ed2630c19f4
Kteshi Sama <igormatias@gmail.com>
2019-06-12 14:38:05 -0700

    File upload ok. index page loading using FS.

--- main/main/main.ino
+++ main/main/main.ino
@@ -1,5 +1,4 @@
 //******************************************************* LIBRARIES ******************************************************
-#include <FastLED.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <WiFiClient.h> 
@@ -7,7 +6,10 @@
 #include <ArduinoOTA.h>
 #include <DNSServer.h>
 #include <ESP8266mDNS.h>
+#include <FastLED.h>
 #include <EEPROM.h>
+#include <FS.h>   // Include the SPIFFS library
+
 
 //************************************************************************************************************************
 //**************************************************** CAPTIVE PORTAL ****************************************************
@@ -21,9 +23,6 @@ const byte DNS_PORT = 53;
 IPAddress apIP(192, 168, 1, 1);
 IPAddress netMsk(255, 255, 255, 0);
 DNSServer dnsServer;
-ESP8266WebServer server(80);
-
-ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
 //************************************************************************************************************************
 //****************************************************    LED STRIP   ****************************************************
 //Configurations
@@ -41,7 +40,7 @@ FASTLED_USING_NAMESPACE
 
 CRGB leds[NUM_LEDS];
 
-#define BRIGHTNESS         255
+#define BRIGHTNESS         92
 #define FRAMES_PER_SECOND  120
 
 int colourRed = 255;
@@ -56,6 +55,7 @@ int OTAport = 8266;
 
 //************************************************************************************************************************
 //*************************************************** GLOBAL VARIABLES ***************************************************
+#define INDEX     "/index.html"
 bool changePattern = false;
 bool changePatternOnline = false;
 int btm_brightness = 255;
@@ -63,10 +63,22 @@ int btm_brightness = 255;
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
+File fsUploadFile;              // a File object to temporarily store the received file
+ESP8266WebServer server(80);
+ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+
+
 //************************************************************************************************************************
 //*****************************************************   WEB PAGE   *****************************************************
-String page ="<!DOCTYPE html> <html><head> <style> html{ text-align:center; font-family: Arial, Helvetica, sans-serif; } a, .button_style { background-color: #008CBA; border: none; color: white; width: 80%; height: 30px; line-height: 30px; float: left; margin-left: 10%; margin-bottom: 15px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; border-radius: 26px; -moz-border-radius: 26px; -webkit-border-radius: 26px; border:none; } </style> </head> <body> <h1>UFO</h1> <p><a href=\"nextMode\">Next Mode</a></p> <form action=\"/colourMode\"> <p> <input type=\"text\" name=\"r\" id=\"r\" value=\"255\"> <input type=\"text\" name=\"g\" id=\"g\" value=\"0\"> <input type=\"text\" name=\"b\" id=\"b\" value=\"0\"> </p> <input type=\"submit\" class=\"button_style\" value=\"Set Colour\" /> </form> <p><a href=\"ledOff\">OFF</a></p> </body></html>";
+String uploadPage ="<html><body><form method=\"post\" enctype=\"multipart/form-data\"><input type=\"file\" name=\"name\"><input class=\"button\" type=\"submit\" value=\"Upload\"></form></body></html>";
 
+
+//************************************************************************************************************************
+//****************************************************  PROTOTYPES  ******************************************************
+//Function Prototypes
+String getContentType(String filename); // convert the file extension to the MIME type
+bool handleFileRead(String path);       // send the right file to the client (if it exists)
+void handleFileUpload(void);            // Save the Uploaded file into the FS memory
 
 //************************************************************************************************************************
 //*******************************************************  SETUP  ********************************************************
@@ -78,7 +90,6 @@ void setup() {
   
   Serial.begin(115200);
   Serial.println("Connecting ...");
-  
   int i = 0;
   while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
     delay(250);
@@ -91,6 +102,15 @@ void setup() {
   Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
 
 
+  if (MDNS.begin("esp8266")) {              // Start the mDNS responder for esp8266.local
+    Serial.println("mDNS responder started");
+  } else {
+    Serial.println("Error setting up MDNS responder!");
+  }
+
+  SPIFFS.begin();                           // Start the SPI Flash Files System
+  
+  
   /*
   WiFi.mode(WIFI_AP);
   Serial.println();
@@ -104,19 +124,29 @@ void setup() {
   
   
   */
-  server.on("/", handleRoot);
+  //server.on("/", handleRoot);
+  
+  server.on("/upload", HTTP_GET, []() {                 // if the client requests the upload page
+    server.send(200, "text/html",uploadPage);
+  });
+
+  server.on("/upload", HTTP_POST,                       // if the client posts to the upload page
+    [](){ server.send(200); },                          // Send status 200 (OK) to tell the client we are ready to receive
+    handleFileUpload                                    // Receive and save the file
+  );
+  
   server.on("/nextMode", handleNextMode);
   server.on("/colourMode", handleColourMode);
   server.on("/ledOff", handleLedOff);
-  server.onNotFound([]() {
-    server.send(200, "text/html", page);
-  });  
+  
+  server.onNotFound([]() {                              // If the client requests any URI
+    if (!handleFileRead(server.uri()))                  // send it if it exists
+      server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
+  });
   
   server.begin();
-  Serial.println("HTTP server started");
-  
+  Serial.println("HTTP server started");  
   delay(2000); // 3 second delay for recovery
-
   
   // tell FastLED about the LED strip configuration
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
@@ -159,6 +189,10 @@ void setup() {
 typedef void (*SimplePatternList[])();
 SimplePatternList gPatterns = { ufo, lights_on, juggle, cylon, LEDFire2012, rainbow, confetti, sinelon, bpm, lights_off };
 
+
+//************************************************************************************************************
+//************************************************************************************************************
+//Main Loop
 void loop()
 {
   ArduinoOTA.handle();
@@ -180,43 +214,72 @@ void loop()
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
+//************************************************************************************************************************
+//********************************************  handleFileUpload FUNCTION  ***********************************************
+//Handle the file read from the server
 
-void handleRoot() {
-  server.send(200, "text/html",page);
+void handleFileUploadPage() {
+  server.send(200, "text/html",uploadPage);
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
+
 void handleNextMode() {
-  server.send(200, "text/html",page);
+  handleFileRead(INDEX);
   changePattern = true;
   nextPattern(); // trigger once 
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
+
 void handleLedOff() {
-  server.send(200, "text/html",page);
+  FastLED.clear();
+  handleFileRead(INDEX);
   gCurrentPatternNumber = (ARRAY_SIZE( gPatterns) - 1);
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
+
 void handleColourMode(){
   colourRed = (server.arg("r") != "") ? server.arg("r").toInt() : 0;
   colourGreen = (server.arg("g") != "") ? server.arg("g").toInt() : 0;
   colourBlue = (server.arg("b") != "") ? server.arg("b").toInt() : 0;
-  server.send(200, "text/html",page);
+  handleFileRead(INDEX);
+//  server.send(200, "text/html",mainPage);
   gCurrentPatternNumber = 1;
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void nextPattern()
 {
   // add one to the current pattern number, and wrap around at the end
   gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void LEDFire2012() 
 {
   Fire2012();
 }
 
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void lights_off() 
 {
-  FastLED.clear ();
+  FastLED.clear();
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void lights_on() 
 {
@@ -226,6 +289,9 @@ void lights_on()
   FastLED.show();
   delay(100);
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void ufo() 
 {
@@ -246,12 +312,20 @@ void ufo()
     delay(5);
   }
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void fadeall() { 
   for(int i = 0; i < NUM_LEDS; i++) { 
     leds[i].nscale8(250); 
   } 
 }
+
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
+
 void cylon()
 {
   static uint8_t hue = 0;
@@ -282,12 +356,18 @@ void cylon()
   }
   
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void rainbow() 
 {
   // FastLED's built-in rainbow generator
   fill_rainbow( leds, NUM_LEDS, gHue, 7);
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void rainbowWithGlitter() 
 {
@@ -295,6 +375,9 @@ void rainbowWithGlitter()
   rainbow();
   addGlitter(80);
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void addGlitter( fract8 chanceOfGlitter) 
 {
@@ -302,6 +385,9 @@ void addGlitter( fract8 chanceOfGlitter)
     leds[ random16(NUM_LEDS) ] += CRGB::White;
   }
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void confetti() 
 {
@@ -310,6 +396,9 @@ void confetti()
   int pos = random16(NUM_LEDS);
   leds[pos] += CHSV( gHue + random8(64), 200, 255);
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void sinelon()
 {
@@ -318,6 +407,9 @@ void sinelon()
   int pos = beatsin16( 13, 0, NUM_LEDS-1 );
   leds[pos] += CHSV( gHue, 255, 192);
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void bpm()
 {
@@ -329,6 +421,9 @@ void bpm()
     leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
   }
 }
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 
 void juggle() {
   // eight colored dots, weaving in and out of sync with each other
@@ -340,6 +435,10 @@ void juggle() {
   }
 }
 
+
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
 // Fire2012 by Mark Kriegsman, July 2012
 // as part of "Five Elements" shown here: http://youtu.be/knWiGsmgycY
 //// 
@@ -427,6 +526,75 @@ void Fire2012()
     CRGB temp = leds[i];
     leds[i] = leds[cell];
     leds[cell] = temp;
+  }
+}
+
+
+//************************************************************************************************************************
+//*********************************************  getContentType FUNCTION  ************************************************
+//return the content type to the server
+String getContentType(String filename){
+  if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".svg")) return "image/svg+xml";  
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+//************************************************************************************************************************
+//*********************************************  handleFileRead FUNCTION  ************************************************
+//Handle the file read from the server
+bool handleFileRead(String path) { // send the right file to the client (if it exists)
+  if (path.endsWith("/")) path += "index.html";         // If a folder is requested, send the index file
+  #ifdef DEBUG
+  Serial.println("handleFileRead: " + path);
+  #endif
+  String contentType = getContentType(path);            // Get the MIME type
+  if (SPIFFS.exists(path)) {                            // If the file exists
+    File file = SPIFFS.open(path, "r");                 // Open it
+    size_t sent = server.streamFile(file, contentType); // And send it to the client
+    file.close();                                       // Then close the file again
+    return true;
+  }
+  #ifdef DEBUG
+  Serial.println("\tFile Not Found");
+  #endif
+  return false;                                         // If the file doesn't exist, return false
+}
+
+//************************************************************************************************************
+//**************************************  handleFileUpload FUNCTION  *****************************************
+//save the new file in the file memory
+void handleFileUpload(){ // upload a new file to the SPIFFS
+  HTTPUpload& upload = server.upload();
+  if(upload.status == UPLOAD_FILE_START){
+    String filename = upload.filename;
+    if(!filename.startsWith("/")) filename = "/"+filename;
+    Serial.print("handleFileUpload Name: "); Serial.println(filename);
+    fsUploadFile = SPIFFS.open(filename, "w");            // Open the file for writing in SPIFFS (create if it doesn't exist)
+    filename = String();
+  } else if(upload.status == UPLOAD_FILE_WRITE){
+    if(fsUploadFile)
+      fsUploadFile.write(upload.buf, upload.currentSize); // Write the received bytes to the file
+  } else if(upload.status == UPLOAD_FILE_END){
+    if(fsUploadFile) {                                    // If the file was successfully created
+      fsUploadFile.close();                               // Close the file again
+      Serial.print("handleFileUpload Size: "); Serial.println(upload.totalSize);
+      server.send(303, "text/plain", "File upload Successful"); // otherwise, respond with a 404 (Not Found) error
+      //server.sendHeader("Location","/success.html");      // Redirect the client to the success page
+      //server.send(303);
+    } else {
+      server.send(500, "text/plain", "500: couldn't create file");
+    }
   }
 }
 
